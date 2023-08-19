f4c478561a578f9e6d766caa6cacbb634ebaabbf
Ashwath <ashu.sannidhi@gmail.com>
2023-08-19 12:25:48 +0530

    4th commit with sever.ts changes

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('server/db.json');
+const path = require('path'); // Import the path module
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // Use path.join to create the absolute path
 const middlewares = jsonServer.defaults();
 const db = require('./db.json');
 const fs = require('fs');
+
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
